1d4d7c78d192d9af620d58d62c619233b7c9394e
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-12 13:55:13 +0200

    Add socket.io support

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import http    from 'http';
 import ip      from 'ip';
+import sockets from 'socket.io';
 
-let app  = express();
+let app    = express();
+let server = http.Server(app);
+let io     = sockets(server);
+
 let port = process.env.PORT || 3000;
 
 app.use('/', express.static(`${__dirname}/public`));
 
-app.listen(port, () => {
+io.on('connection', socket => {
+    console.log('New socket connected');
+});
+
+server.listen(port, () => {
     console.log(`Motivaationarsu running at http://${ip.address()}:${port}`);
 });
 
