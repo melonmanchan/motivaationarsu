a10002fc19f26318621d11cadb60e307df43ba4f
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-13 17:09:25 +0200

    Add online user count

--- socketio.js
+++ socketio.js
@@ -1,20 +1,20 @@
 import sockets from 'socket.io';
 
 const SOCKET_EVENTS = {
-    DISCONNECT   : 'disconnect',
-    CONNECTION   : 'connection',
-    MOTIVATE     : 'motivate',
-    NEW_MOTIVATE : 'new_motivate'
+    DISCONNECT              : 'disconnect',
+    CONNECTION              : 'connection',
+    MOTIVATE                : 'motivate',
+    NEW_MOTIVATE            : 'new_motivate',
+    MOTIVATOR_AMOUNT_CHANGE : 'motivator_state_change'
 };
 
 let currentMotivations = 0;
+let currentMotivators  = 0;
 
 function setupSocketIO(server) {
     let io = sockets(server);
 
     io.on(SOCKET_EVENTS.CONNECTION, socket => {
-
-        console.log('New socket connected');
 
         socket.emit(SOCKET_EVENTS.MOTIVATE, { currentMotivations: currentMotivations });
 
@@ -22,7 +22,16 @@ function setupSocketIO(server) {
             currentMotivations++;
             io.emit(SOCKET_EVENTS.NEW_MOTIVATE, { currentMotivations: currentMotivations, sentence: payload.sentence });
         });
+
+        socket.on(SOCKET_EVENTS.DISCONNECT, () => {
+            currentMotivators--;
+            io.emit(SOCKET_EVENTS.MOTIVATOR_AMOUNT_CHANGE, { currentMotivators: currentMotivators });
+        });
+
+        currentMotivators++;
+        io.emit(SOCKET_EVENTS.MOTIVATOR_AMOUNT_CHANGE, { currentMotivators: currentMotivators });
     });
+
 };
 
 export default setupSocketIO;
